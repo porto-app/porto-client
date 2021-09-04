d215bfb9e5a62d9f9f9a8e6e9808b81e3d3ccf7f
Kimberlybree <86509310+Kimberlybree@users.noreply.github.com>
2021-09-04 12:18:15 -0400

    First Name Last Name user login

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { InputGroup, FormControl } from 'react-bootstrap'
+
 
 function Profile(props) {
     return (
         <div>
             <h2>User profile will go here!</h2>
+
+            <InputGroup className="mb-3">
+            <InputGroup.Text>First and last name</InputGroup.Text>
+            <FormControl aria-label="First name" />
+            <FormControl aria-label="Last name" />
+            </InputGroup>
         </div>
     );
 }
