aac94cdfa257541641e838b6aeed6048e01fa68b
andyjzhong <andyjzhong@gmail.com>
2021-09-05 12:06:05 -0400

    add back in Router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './Components/Header';
 import Navi from './Components/Navi';
 import Main from './Components/Main';
@@ -12,19 +12,21 @@ import Footer from './Components/Footer.jsx';
 
 
 function App() {
-  return (
-    <div>
-     <DataContext.Provider>
-        <Header />
-        <Navi />
-        <Route exact path='/' component={Main} />
-        <Route exact path='/about' component={About} />
-        <Route exact path='/profile' component={Profile} />
-        <Route exact path='/searchresults' component={SearchResults} />
-        <Footer />
-      </DataContext.Provider>
-    </div>
-  );
+    return (
+        <div>
+            <DataContext.Provider>
+                <Router>
+                <Header />
+                <Navi />
+                <Route exact path='/' component={Main} />
+                <Route exact path='/about' component={About} />
+                <Route exact path='/profile' component={Profile} />
+                <Route exact path='/searchresults' component={SearchResults} />
+                <Footer />
+                </Router>
+            </ DataContext.Provider>
+        </div>
+    );
 }
 
 export default App;
