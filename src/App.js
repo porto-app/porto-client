6d9e30677843ba3124aeb385eb2625c4d56222fe
andyjzhong <andyjzhong@gmail.com>
2021-09-05 11:56:15 -0400

    add useContext component and import to App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Route } from 'react-router-dom';
 import Header from './Components/Header';
 import Navi from './Components/Navi';
 import Main from './Components/Main';
 import About from './Components/About'
 import Profile from './Components/Profile'
 import SearchResults from './Components/SearchResults';
-// import Footer from './Components/Footer.jsx';
+import { DataContext } from './Components/DataContext';
+import Footer from './Components/Footer.jsx';
 
 
 function App() {
   return (
     <div>
-     <Router>
+     <DataContext.Provider>
         <Header />
         <Navi />
         <Route exact path='/' component={Main} />
         <Route exact path='/about' component={About} />
         <Route exact path='/profile' component={Profile} />
         <Route exact path='/searchresults' component={SearchResults} />
-      </Router>
+        <Footer />
+      </DataContext.Provider>
     </div>
   );
 }
