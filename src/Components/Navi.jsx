ee522a5fd52b21281a364feec940e61e424b012f
andyjzhong <andyjzhong@gmail.com>
2021-09-05 20:57:14 -0400

    add new routes to create profile page

--- src/Components/Navi.jsx
+++ src/Components/Navi.jsx
@@ -5,16 +5,16 @@ import { Navbar, Nav, Container } from 'react-bootstrap'
 function Navi() {
 
     return (
-    <div className="navigation">
-     <Navbar bg="primary" variant="dark">
-    <Container>
-    <Nav className="me-auto">
-      <Nav.Link href="/">Home</Nav.Link>
-      <Nav.Link href="/about">About</Nav.Link>
-      <Nav.Link href="/profile">My Profile</Nav.Link>
-    </Nav>
-    </Container>
-  </Navbar>
+        <div className="navigation">
+            <Navbar bg="primary" variant="dark">
+                <Container>
+                    <Nav className="me-auto">
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/about">About</Nav.Link>
+                        <Nav.Link href="/profile">My Profile</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
         </div>
     )
 }
