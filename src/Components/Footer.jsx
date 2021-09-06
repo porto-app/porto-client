9ed9f4e841d1549d3b68d5db4eba5523ae8bca0d
zrehbein <zrehbein@gmail.com>
2021-09-06 13:33:08 -0700

    added footer with placeholder link and text

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Footer(props) {
     return (
-        <div>
-            
+        <div className="footer">
+             <Link to='/'><h1 className="main-footer-title">Porto</h1></Link>
+           <p>Built by: Andy, Kim, Lawrence, Roxie, and Zak</p>
+           <h4>Copyright 2021</h4>
         </div>
     );
 }
