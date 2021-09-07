018437a1e51b44758c95340d03e2a3fa49214483
Kimberlybree <86509310+Kimberlybree@users.noreply.github.com>
2021-09-07 11:03:11 -0400

    ProjectsCards

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { Button, Modal, Form } from 'react-bootstrap';
+import { Button, Modal, Form, Card, Placeholder } from 'react-bootstrap';
 import { DataContext } from './DataContext'
 import { Link } from 'react-router-dom';
 import axios from 'axios';
@@ -127,7 +127,42 @@ function Profile(props) {
             <Link to={`/searchresults`} key={props.id}>
                 <Button variant="danger" onClick={handleDelete}>Delete Profile</Button>
             </Link>
+
+
+
+            <div className="projectsCard">
+                <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+        Some quick example text to build on the card title and make up the bulk
+        of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+        </Card>
+
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+            <Placeholder as={Card.Title} animation="glow">
+                <Placeholder xs={6} />
+            </Placeholder>
+            <Placeholder as={Card.Text} animation="glow">
+                <Placeholder xs={7} /> <Placeholder xs={4} /> <Placeholder xs={4} />{' '}
+                <Placeholder xs={6} /> <Placeholder xs={8} />
+            </Placeholder>
+            <Placeholder.Button variant="primary" xs={6} />
+            </Card.Body>
+        </Card>
         </div>
+            </div>
+
+        
+
+
+
     );
 }
 
