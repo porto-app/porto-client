88645db6b650e26be4bd0f3052dead2746f29622
zrehbein <zrehbein@gmail.com>
2021-09-09 12:13:19 -0700

    required placeholders

--- src/Components/CreateProfile.jsx
+++ src/Components/CreateProfile.jsx
@@ -170,105 +170,106 @@ function CreateProfile(props) {
                     <Row>
                     <Col>
                     <Form.Group className="mb-3" controlId="formGroupEmail">
-                        <Form.Label>Email</Form.Label>
+                        <Form.Label>Email*</Form.Label>
                         <Form.Control type="email" placeholder="Enter email" onChange={storeEmail}/>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formGroupFirstName">
-                        <Form.Label>First Name</Form.Label>
-                        <Form.Control onChange={storeFirstName}/>
+                        <Form.Label>First Name*</Form.Label>
+                        <Form.Control placeholder="First Name" onChange={storeFirstName}/>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formGroupMiddleName">
                         <Form.Label>Middle Name</Form.Label>
-                        <Form.Control onChange={storeMiddleName}/>
+                        <Form.Control placeholder="Middle Name" onChange={storeMiddleName}/>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formGroupLastName">
-                        <Form.Label>Last Name</Form.Label>
-                        <Form.Control onChange={storeLastName}/>
+                        <Form.Label>Last Name*</Form.Label>
+                        <Form.Control placeholder="Last Name" onChange={storeLastName}/>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formGroupTitle">
-                        <Form.Label>Title</Form.Label>
-                        <Form.Control onChange={storeTitle}/>
+                        <Form.Label>Title*</Form.Label>
+                        <Form.Control placeholder="Title" onChange={storeTitle}/>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formGroupLocation">
-                        <Form.Label>Location</Form.Label>
-                        <Form.Control onChange={storeLocation}/>
+                        <Form.Label>Location*</Form.Label>
+                        <Form.Control placeholder="Location" onChange={storeLocation}/>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formGroupProfilePic">
-                        <Form.Label>Profile Pic URL</Form.Label>
-                        <Form.Control onChange={storeUrlPic}/>
+                        <Form.Label>Profile Pic URL*</Form.Label>
+                        <Form.Control placeholder="Profile Pic URL" onChange={storeUrlPic}/>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formGroupUrlResume">
-                        <Form.Label>Resume URL</Form.Label>
-                        <Form.Control onChange={storeUrlResume}/>
+                        <Form.Label>Resume URL*</Form.Label>
+                        <Form.Control placeholder="Resume URL" onChange={storeUrlResume}/>
                     </Form.Group>
                     </Col>
                     <Col>
                     <Form.Group className="mb-3" controlId="formGroupP1Name">
-                        <Form.Label>Project 1 - Name</Form.Label>
-                        <Form.Control onChange={storeProject1Name} />
+                        <Form.Label>Project 1 - Name*</Form.Label>
+                        <Form.Control placeholder="Project 1 Name" onChange={storeProject1Name} />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formGroupP1Desc">
-                        <Form.Label>Project 1 - Description</Form.Label>
-                        <Form.Control onChange={storeProject1Description} />
+                        <Form.Label>Project 1 - Description*</Form.Label>
+                        <Form.Control placeholder="Project 1 Description" onChange={storeProject1Description} />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formGroupP1Url">
-                        <Form.Label>Project 1 - URL</Form.Label>
-                        <Form.Control onChange={storeProject1URL} />
+                        <Form.Label>Project 1 - URL*</Form.Label>
+                        <Form.Control placeholder="Project 1 URL" onChange={storeProject1URL} />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formGroupP1Image">
                         <Form.Label>Project 1 - Image URL</Form.Label>
-                        <Form.Control onChange={storeProject1Img} />
+                        <Form.Control placeholder="Project 1 Image URL" onChange={storeProject1Img} />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formGroupP2Name">
                         <Form.Label>Project 2 - Name</Form.Label>
-                        <Form.Control onChange={storeProject2Name} />
+                        <Form.Control placeholder="Project 2 Name" onChange={storeProject2Name} />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formGroupP2Desc">
                         <Form.Label>Project 2 - Description</Form.Label>
-                        <Form.Control onChange={storeProject2Description} />
+                        <Form.Control placeholder="Project 2 Description" onChange={storeProject2Description} />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formGroupP2Url">
                         <Form.Label>Project 2 - URL</Form.Label>
-                        <Form.Control onChange={storeProject2URL} />
+                        <Form.Control placeholder="Project 2 URL" onChange={storeProject2URL} />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formGroupP2Image">
                         <Form.Label>Project 2 - Image URL</Form.Label>
-                        <Form.Control onChange={storeProject2Img} />
+                        <Form.Control placeholder="Project 2 Image URL" onChange={storeProject2Img} />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formGroupP3Name">
                         <Form.Label>Project 3 - Name</Form.Label>
-                        <Form.Control onChange={storeProject3Name} />
+                        <Form.Control placeholder="Project 3 Name" onChange={storeProject3Name} />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formGroupP3Desc">
                         <Form.Label>Project 3 - Description</Form.Label>
-                        <Form.Control onChange={storeProject3Description} />
+                        <Form.Control placeholder="Project 3 Description" onChange={storeProject3Description} />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formGroupP3Url">
                         <Form.Label>Project 3 - URL</Form.Label>
-                        <Form.Control onChange={storeProject3URL} />
+                        <Form.Control placeholder="Project 3 URL" onChange={storeProject3URL} />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formGroupP3Image">
                         <Form.Label>Project 3 - Image URL</Form.Label>
-                        <Form.Control onChange={storeProject3Img} />
+                        <Form.Control placeholder="Project 3 Image URL" onChange={storeProject3Img} />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formGroupP4Name">
                         <Form.Label>Project 4 - Name</Form.Label>
-                        <Form.Control onChange={storeProject4Name} />
+                        <Form.Control placeholder="Project 4 Name" onChange={storeProject4Name} />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formGroupP4Desc">
                         <Form.Label>Project 4 - Description</Form.Label>
-                        <Form.Control onChange={storeProject4Description} />
+                        <Form.Control placeholder="Project 4 Description" onChange={storeProject4Description} />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formGroupP4Url">
                         <Form.Label>Project 4 - URL</Form.Label>
-                        <Form.Control onChange={storeProject4URL} />
+                        <Form.Control placeholder="Project 4 URL" onChange={storeProject4URL} />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formGroupP4Image">
                         <Form.Label>Project 4 - Image URL</Form.Label>
-                        <Form.Control onChange={storeProject4Img} />
+                        <Form.Control placeholder="Project 4 Image URL" onChange={storeProject4Img} />
                     </Form.Group>
                     </Col>
                     </Row>
+                    <p>* denotes required field</p>
                     <Link to='/searchResults'>
                         <Button variant="primary" type="submit" onClick={routeToSearch}>Submit</Button>
                     </Link>
