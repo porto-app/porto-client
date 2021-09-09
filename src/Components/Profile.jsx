4d307a325f75fd34c0ced065e6c242fcb1b1737d
andyjzhong <andyjzhong@gmail.com>
2021-09-08 21:31:13 -0400

    add edit modals for each project

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -21,10 +21,31 @@ function Profile(props) {
         e.preventDefault();
         setP1Show(true)
     };
+    // P2 Modal Commands
+    const [p2show, setP2Show] = useState(false);
+    const p2handleClose = () => setP2Show(false);
+    const p2handleShow = (e) => {
+        e.preventDefault();
+        setP2Show(true)
+    };
+    // P3 Modal Commands
+    const [p3show, setP3Show] = useState(false);
+    const p3handleClose = () => setP3Show(false);
+    const p3handleShow = (e) => {
+        e.preventDefault();
+        setP3Show(true)
+    };
+    // P4 Modal Commands
+    const [p4show, setP4Show] = useState(false);
+    const p4handleClose = () => setP4Show(false);
+    const p4handleShow = (e) => {
+        e.preventDefault();
+        setP4Show(true)
+    };
 
     useEffect(() => {
         getOneProfile()
-    }, [show, p1show])
+    }, [show, p1show, p2show, p3show, p4show])
 
     const getOneProfile = async () => {
         console.log("Attempting to retrieve one profile...")
@@ -111,6 +132,81 @@ function Profile(props) {
         }
     }
 
+    const submitP2Edit = async () => {
+
+        let project2DescriptionEdited = document.querySelector(".project2DescriptionEdit").value
+        let project2NameEdited = document.querySelector(".project2NameEdit").value
+        let project2ImgEdited = document.querySelector(".project2ImgEdit").value
+        let project2URLEdited = document.querySelector(".project2URLEdit").value
+
+        try {
+            const url =
+                process.env.NODE_ENV === 'production'
+                    ? `http://porto-app-server.herokuapp.com/profiles/${currentProfileId}`
+                    : `http://localhost:5000/profiles/${currentProfileId}`
+
+            axios.put(url, {
+                Project2Description: project2DescriptionEdited,
+                Project2Name: project2NameEdited,
+                Project2Img: project2ImgEdited,
+                Project2URL: project2URLEdited
+            });
+            console.log("Edit successful!");
+        } catch (error) {
+            console.warn("Error when editing one profile.")
+        }
+    }
+
+    const submitP3Edit = async () => {
+
+        let project3DescriptionEdited = document.querySelector(".project3DescriptionEdit").value
+        let project3NameEdited = document.querySelector(".project3NameEdit").value
+        let project3ImgEdited = document.querySelector(".project3ImgEdit").value
+        let project3URLEdited = document.querySelector(".project3URLEdit").value
+
+        try {
+            const url =
+                process.env.NODE_ENV === 'production'
+                    ? `http://porto-app-server.herokuapp.com/profiles/${currentProfileId}`
+                    : `http://localhost:5000/profiles/${currentProfileId}`
+
+            axios.put(url, {
+                Project3Description: project3DescriptionEdited,
+                Project3Name: project3NameEdited,
+                Project3Img: project3ImgEdited,
+                Project3URL: project3URLEdited
+            });
+            console.log("Edit successful!");
+        } catch (error) {
+            console.warn("Error when editing one profile.")
+        }
+    }
+
+    const submitP4Edit = async () => {
+
+        let project4DescriptionEdited = document.querySelector(".project4DescriptionEdit").value
+        let project4NameEdited = document.querySelector(".project4NameEdit").value
+        let project4ImgEdited = document.querySelector(".project4ImgEdit").value
+        let project4URLEdited = document.querySelector(".project4URLEdit").value
+
+        try {
+            const url =
+                process.env.NODE_ENV === 'production'
+                    ? `http://porto-app-server.herokuapp.com/profiles/${currentProfileId}`
+                    : `http://localhost:5000/profiles/${currentProfileId}`
+
+            axios.put(url, {
+                Project4Description: project4DescriptionEdited,
+                Project4Name: project4NameEdited,
+                Project4Img: project4ImgEdited,
+                Project4URL: project4URLEdited
+            });
+            console.log("Edit successful!");
+        } catch (error) {
+            console.warn("Error when editing one profile.")
+        }
+    }
+
     return (
         <div className="profile-page">
             <Modal show={show} onHide={handleClose}>
@@ -181,6 +277,102 @@ function Profile(props) {
                 </Modal.Footer>
             </Modal>
 
+            <Modal show={p2show} onHide={p2handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Edit Project 2</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form.Group className="mb-3" controlId="formGroupP1Name">
+                        <Form.Label>Project 2 Name</Form.Label>
+                        <Form.Control className="project2NameEdit" defaultValue={activeProfile.Project2Name}/>
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formGroupP2Desc">
+                        <Form.Label>Project 2 Description</Form.Label>
+                        <Form.Control className="project2DescriptionEdit" defaultValue={activeProfile.Project2Description}/>
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formGroupP2Image">
+                        <Form.Label>Project 2 Image URL</Form.Label>
+                        <Form.Control className="project2ImgEdit" defaultValue={activeProfile.Project2Img}/>
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formGroupP2Url">
+                        <Form.Label>Project 2 Deployed URL</Form.Label>
+                        <Form.Control className="project2URLEdit" defaultValue={activeProfile.Project2URL}/>
+                    </Form.Group>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={p2handleClose}>Close</Button>
+                    <Button variant="primary" onClick={() => {
+                        submitP2Edit()
+                        p2handleClose()
+                        getOneProfile()
+                    }}>Save Changes</Button>
+                </Modal.Footer>
+            </Modal>
+
+            <Modal show={p3show} onHide={p3handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Edit Project 3</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form.Group className="mb-3" controlId="formGroupP1Name">
+                        <Form.Label>Project 3 Name</Form.Label>
+                        <Form.Control className="project3NameEdit" defaultValue={activeProfile.Project3Name}/>
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formGroupP3Desc">
+                        <Form.Label>Project 3 Description</Form.Label>
+                        <Form.Control className="project3DescriptionEdit" defaultValue={activeProfile.Project3Description}/>
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formGroupP3Image">
+                        <Form.Label>Project 3 Image URL</Form.Label>
+                        <Form.Control className="project3ImgEdit" defaultValue={activeProfile.Project3Img}/>
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formGroupP3Url">
+                        <Form.Label>Project 3 Deployed URL</Form.Label>
+                        <Form.Control className="project3URLEdit" defaultValue={activeProfile.Project3URL}/>
+                    </Form.Group>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={p3handleClose}>Close</Button>
+                    <Button variant="primary" onClick={() => {
+                        submitP3Edit()
+                        p3handleClose()
+                        getOneProfile()
+                    }}>Save Changes</Button>
+                </Modal.Footer>
+            </Modal>
+
+            <Modal show={p4show} onHide={p4handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Edit Project 4</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form.Group className="mb-4" controlId="formGroupP1Name">
+                        <Form.Label>Project 4 Name</Form.Label>
+                        <Form.Control className="project4NameEdit" defaultValue={activeProfile.Project4Name}/>
+                    </Form.Group>
+                    <Form.Group className="mb-4" controlId="formGroupP4Desc">
+                        <Form.Label>Project 4 Description</Form.Label>
+                        <Form.Control className="project4DescriptionEdit" defaultValue={activeProfile.Project4Description}/>
+                    </Form.Group>
+                    <Form.Group className="mb-4" controlId="formGroupP4Image">
+                        <Form.Label>Project 4 Image URL</Form.Label>
+                        <Form.Control className="project4ImgEdit" defaultValue={activeProfile.Project4Img}/>
+                    </Form.Group>
+                    <Form.Group className="mb-4" controlId="formGroupP4Url">
+                        <Form.Label>Project 4 Deployed URL</Form.Label>
+                        <Form.Control className="project4URLEdit" defaultValue={activeProfile.Project4URL}/>
+                    </Form.Group>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={p4handleClose}>Close</Button>
+                    <Button variant="primary" onClick={() => {
+                        submitP4Edit()
+                        p4handleClose()
+                        getOneProfile()
+                    }}>Save Changes</Button>
+                </Modal.Footer>
+            </Modal>
+
             <div className="myProfile">
                 <img className="pic" src={activeProfile.urlPic} alt="" />
                 <h2>
@@ -216,7 +408,7 @@ function Profile(props) {
                         <Card.Title>{activeProfile.Project2Name}</Card.Title>
                         <Card.Text>{activeProfile.Project2Description}</Card.Text>
                         <a href={activeProfile.Project2URL}>
-                            <Button variant="primary">Click here</Button>
+                            <Button variant="primary" onClick={p2handleShow}>Edit Project</Button>
                         </a>
                     </Card.Body>
                 </Card>
@@ -229,7 +421,7 @@ function Profile(props) {
                         <Card.Title>{activeProfile.Project3Name}</Card.Title>
                         <Card.Text>{activeProfile.Project3Description}</Card.Text>
                         <a href={activeProfile.Project3URL}>
-                            <Button variant="primary">Click here</Button>
+                            <Button variant="primary" onClick={p3handleShow}>Edit Project</Button>
                         </a>
                     </Card.Body>
                 </Card>
@@ -242,7 +434,7 @@ function Profile(props) {
                         <Card.Title>{activeProfile.Project4Name}</Card.Title>
                         <Card.Text>{activeProfile.Project4Description}</Card.Text>
                         <a href={activeProfile.Project4URL}>
-                            <Button variant="primary">Click here</Button>
+                            <Button variant="primary" onClick={p4handleShow}>Edit Project</Button>
                         </a>
                     </Card.Body>
                 </Card>
